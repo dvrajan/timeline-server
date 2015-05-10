3bc5a2cca0935676875239d4b22b28a0750a9251
Deva <dvrajan12@gmail.com>
2015-05-10 08:55:13 +0530

    generating random events

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var restify = require('restify')
     , store = require('./store')
+    , _ = require('./lib/lodash')
     , noOfPostsPerPage = 10;
 
 var server = restify.createServer({
@@ -17,13 +18,42 @@ server.use(
   }
 );
 
+function filterEvents(events){
+  var filteredEvents = [];
+  var d = new Date(events[0].date);
+  var res = [];
+  var trial = 0;
+  do{
+    res = _.filter(events, _.matches({date: d}));
+    filteredEvents = filteredEvents.concat(threeRandomValues(res));
+    d.setDate(d.getDate() - 1);
+    if(res.length == 0){
+      trial ++;
+    }
+  } while(trial < 10);
+
+
+  return filteredEvents;
+}
+
+function threeRandomValues(arr){
+  var res = [];
+  for(var i=0; i<3 ; i++){
+    var value = Math.floor((Math.random() * arr.length) + 1);
+    if(res.indexOf(arr[value]) < 0 && arr[value] != null){
+        res.push(arr[value]);
+    }
+  }
+  return res;
+}
+
 server.get('/events', function (req, res, next) {
   store.getEvents(function(events){
+    var filteredEvents = filterEvents(events);
     var response = {
-      data: events
+      data: filteredEvents
     }
     res.send(response);
-
   });
   return next();
 });
