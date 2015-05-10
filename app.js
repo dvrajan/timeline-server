17cce2a597faa828104c8e4b3273f68fe0c93c75
Deva <dvrajan12@gmail.com>
2015-05-10 10:23:57 +0530

    moving to express to host static files

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-var restify = require('restify')
+var express = require('express')
+    , app = express()
     , store = require('./store')
-    , _ = require('./lib/lodash')
-    , noOfPostsPerPage = 10;
+    , _ = require('./lib/lodash');
 
-var server = restify.createServer({
-  name: 'timeline',
-  version: '1.0.0'
-});
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
-server.use(
-  function crossOrigin(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    return next();
-  }
-);
 
 function filterEvents(events){
   var filteredEvents = [];
@@ -47,7 +33,8 @@ function threeRandomValues(arr){
   return res;
 }
 
-server.get('/events', function (req, res, next) {
+app.use(express.static(__dirname + '/public'));
+app.get('/events', function (req, res) {
   store.getEvents(function(events){
     var filteredEvents = filterEvents(events);
     var response = {
@@ -55,9 +42,14 @@ server.get('/events', function (req, res, next) {
     }
     res.send(response);
   });
-  return next();
 });
 
-server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
+
+var server = app.listen(80, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Timeline app listening at http://%s:%s', host, port);
+
 });
--- public/user-events.js
+++ public/user-events.js
@@ -1,26 +1,26 @@
 var app = angular.module('timelineApp', []);
 app.controller('UserEventController', function($scope, $http) {
-	
+
 	$scope.fetchEvents = function() {
 		$('#loader').show();
-		
+
 		FB.api('/me/posts?limit=10000', function(response) {
 			var events = [];
-    	for (i = 0; i < response.data.length; i++) { 
+    	for (i = 0; i < response.data.length; i++) {
     		if (response.data[i].story && response.data[i].created_time ) {
     			fmtDate = $scope.getDate(response.data[i].created_time);
     			event =  {title:response.data[i].story, date:fmtDate , picture:response.data[i].picture, description:response.data[i].description, type:"user"};
-    			events.push(event);	
+    			events.push(event);
     		}
-    		
+
 		}
 			if ($scope.events) {
 				$scope.events = $scope.events.concat(events);
 			}else {
-				$scope.events = events;	
+				$scope.events = events;
 			}
 
-				$http.get("http://192.168.25.103:8080/events").success(function(response){
+				$http.get("http://104.155.102.248/events").success(function(response){
 
     			for ( i =0;i< response.data.length; i++) {
     				response.data[i].type = "other";
@@ -29,7 +29,7 @@ app.controller('UserEventController', function($scope, $http) {
     			if ($scope.events) {
 					$scope.events = $scope.events.concat(response.data);
 				}else {
-					$scope.events = response.data;	
+					$scope.events = response.data;
 				}
 				$scope.events.sort(function(a,b){
 				var c = new Date(a.date);
@@ -42,15 +42,15 @@ app.controller('UserEventController', function($scope, $http) {
 				$('#loader').hide();
 
 		});
-		
+
 		var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 		$scope.getDate = function(dateStr) {
 			date = new Date(dateStr);
 
 			return date.getDate() + " " + months[date.getMonth()] +" "+date.getFullYear();
 		};
-		
-		
+
+
 
 
 	};
